2fb90decec3384a2d97d663d654ecc02fde4b8ef
Arhita <91649565+ArhitaJain@users.noreply.github.com>
2022-03-16 12:43:16 +0530

    Add files via upload

---
+++ laptop.js
@@ -0,0 +1,30 @@
+Status = "";
+laptop_image = "";
+
+function preload(){
+    laptop_image = loadImage("macbook-pro-2021-cnet-review-22.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.center();
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(laptop_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(laptop_image,0,0,640,350);
+}
